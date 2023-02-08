baae837eac7dd34bbd0d3b5e687f62bdc2b9a393
Santeri Leinonen <leinosan@uef.fi>
2023-02-08 10:23:18 +0200

    muutos

--- app/src/index.js
+++ app/src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import RouteApp from './router';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import RouteApp from "./router";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <RouteApp />
-  </React.StrictMode>
+    <React.StrictMode>
+        <RouteApp />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
