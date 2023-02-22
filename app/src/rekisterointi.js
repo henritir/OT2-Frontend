ec78821a3db5cb6940d348ac1c0f2ba9cc99684a
Santeri Leinonen <leinosan@uef.fi>
2023-02-22 10:06:26 +0200

    rek lisää

--- app/src/rekisterointi.js
+++ app/src/rekisterointi.js
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 const Rekisterointi = () => {
+    const { boolean, setBoolean } = useState(false);
+
     return (
         <div>
             <div
@@ -6,66 +10,96 @@ const Rekisterointi = () => {
                     minHeight: "20vh",
                 }}
             ></div>
+            {boolean ? (
+                <div>
+                    <div
+                        style={{
+                            minHeight: "20vh",
+                        }}
+                    ></div>
+                    <div
+                        style={{
+                            backgroundColor: "rgba(255, 255, 255, 1)",
+                            marginLeft: "40%",
+                            marginRight: "40%",
+                            width: "400px",
+                            minHeight: "330px",
+                        }}
+                    >
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <br></br>
 
-            <div
-                style={{
-                    backgroundColor: "rgba(255, 255, 255, 0.9)",
-                    marginLeft: "40%",
-                    marginRight: "40%",
-                    width: "400px",
-                    minHeight: "330px",
-                }}
-            >
-                <form action="/onnistui">
-                    <br></br>
-                    <div>
-                        <h1>Rekisteröidy tässä</h1>
+                        <div>
+                            <h1>Rekisteröityminen onnistui</h1>
+                        </div>
                     </div>
-                    <br></br>
-                    <div>
-                        <input
-                            id="sahkoposti"
-                            type={"email"}
-                            placeholder={"Sähköposti"}
-                            style={{ borderRadius: "6px" }}
-                            required
-                        ></input>
-                    </div>
-                    <br></br>
-                    <div>
-                        <input
-                            id="tunnus"
-                            type={"text"}
-                            placeholder={"Käyttäjätunnus"}
-                            style={{ borderRadius: "6px" }}
-                            required
-                        ></input>
-                    </div>
-                    <br></br>
-                    <div>
-                        <input
-                            id="salasana"
-                            type={"password"}
-                            placeholder={"Salasana"}
-                            style={{ borderRadius: "6px" }}
-                            required
-                        ></input>
-                    </div>
-                    <br></br>
-                    <div>
-                        <button
-                            id="nappi"
-                            type="submit"
-                            style={{
-                                borderRadius: "6px",
-                                backgroundColor: "gray",
-                            }}
-                        >
-                            Rekisteröidy
-                        </button>
-                    </div>
-                </form>
-            </div>
+                </div>
+            ) : (
+                <div
+                    style={{
+                        backgroundColor: "rgba(255, 255, 255, 0.9)",
+                        marginLeft: "40%",
+                        marginRight: "40%",
+                        width: "400px",
+                        minHeight: "330px",
+                    }}
+                >
+                    <form action="/onnistui">
+                        <br></br>
+                        <div>
+                            <h1>Rekisteröidy tässä</h1>
+                        </div>
+                        <br></br>
+                        <div>
+                            <input
+                                id="sahkoposti"
+                                type={"email"}
+                                placeholder={"Sähköposti"}
+                                style={{ borderRadius: "6px" }}
+                                required
+                            ></input>
+                        </div>
+                        <br></br>
+                        <div>
+                            <input
+                                id="tunnus"
+                                type={"text"}
+                                placeholder={"Käyttäjätunnus"}
+                                style={{ borderRadius: "6px" }}
+                                required
+                            ></input>
+                        </div>
+                        <br></br>
+                        <div>
+                            <input
+                                id="salasana"
+                                type={"password"}
+                                placeholder={"Salasana"}
+                                style={{ borderRadius: "6px" }}
+                                required
+                            ></input>
+                        </div>
+                        <br></br>
+                        <div>
+                            <button
+                                id="nappi"
+                                type="submit"
+                                style={{
+                                    borderRadius: "6px",
+                                    backgroundColor: "gray",
+                                }}
+                                onClick={() => {
+                                    setBoolean(true);
+                                }}
+                            >
+                                Rekisteröidy
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            )}
         </div>
     );
 };
