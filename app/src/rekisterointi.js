8bec854c805618d8d5156b12dd99c8719d1c52df
Santeri Leinonen <leinosan@uef.fi>
2023-04-21 17:16:25 +0300

    Rekisterointi tilanne ilmoitukset

--- app/src/rekisterointi.js
+++ app/src/rekisterointi.js
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
-import { Link, Navigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import Nav from "react-bootstrap/Nav";
+import Snackbar from "@mui/material/Snackbar";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
 
 const Rekisterointi = () => {
     const [sposti, setSposti] = useState("");
     const [kayttajanimi, setKayttajanimi] = useState("");
     const [salasana, setSalasana] = useState("");
+    const [openSnackBar, setOpenSnackBar] = useState(false);
+    const [openSnackBar1, setOpenSnackBar1] = useState(false);
+    const [openSnackBar2, setOpenSnackBar2] = useState(false);
 
     const postRekisterointi = () => {
         fetch("http://localhost:3001/rekisteroidy/", {
@@ -19,13 +25,13 @@ const Rekisterointi = () => {
                 "Content-Type": "application/json",
             },
         })
-            .then((res) => res.json())
-            .then((data) => {
-                if (data.error_message) {
-                    alert(data.error_message);
+            .then((response) => {
+                if (response.status === 400) {
+                    setOpenSnackBar1(true);
+                } else if (response.status === 500) {
+                    setOpenSnackBar2(true);
                 } else {
-                    alert(data.message);
-                    <Navigate to="/" />;
+                    setOpenSnackBar(true);
                 }
             })
             .catch((err) => console.error(err));
@@ -38,6 +44,30 @@ const Rekisterointi = () => {
         setSalasana("");
         setSposti("");
     };
+
+    // Arvostelun päivittämisen onnistumisesta ilmoittavan snackbarin sulkeminen
+    const handleCloseSnackBar = (event, reason) => {
+        if (reason === "clickaway") {
+            return;
+        }
+        setOpenSnackBar(false);
+        setOpenSnackBar1(false);
+        setOpenSnackBar2(false);
+    };
+
+    //Snackbarin X-Button
+    const snackBarAction = (
+        <React.Fragment>
+            <IconButton
+                size="small"
+                aria-label="close"
+                color="inherit"
+                onClick={handleCloseSnackBar}
+            >
+                <CloseIcon fontSize="small" />
+            </IconButton>
+        </React.Fragment>
+    );
 
     return (
         <div>
@@ -118,106 +148,29 @@ const Rekisterointi = () => {
                     </form>
                 </div>
             </div>
+            <Snackbar
+                open={openSnackBar}
+                autoHideDuration={6000}
+                onClose={handleCloseSnackBar}
+                message="Rekisteröityminen onnistui!"
+                action={snackBarAction}
+            />
+            <Snackbar
+                open={openSnackBar1}
+                autoHideDuration={6000}
+                onClose={handleCloseSnackBar}
+                message="Käyttäjänimi tai sähköposti on jo käytössä."
+                action={snackBarAction}
+            />
+            <Snackbar
+                open={openSnackBar2}
+                autoHideDuration={6000}
+                onClose={handleCloseSnackBar}
+                message="Käyttäjän lisäys epäonnistui."
+                action={snackBarAction}
+            />
         </div>
     );
 };
 
 export { Rekisterointi };
-
-/* {boolen ? (        <div>
-            <div
-                style={{
-                    minHeight: "20vh",
-                }}
-            ></div>
-            <div
-                style={{
-                    backgroundColor: "rgba(255, 255, 255, 1)",
-                    marginLeft: "40%",
-                    marginRight: "40%",
-                    width: "400px",
-                    minHeight: "330px",
-                }}
-            >
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-
-                <div>
-                    <h1>Rekisteröityminen onnistui</h1>
-                </div>
-            </div>
-        </div>):(<div
-                style={{
-                    backgroundColor: "rgba(255, 255, 255, 0.9)",
-                    marginLeft: "40%",
-                    marginRight: "40%",
-                    width: "400px",
-                    minHeight: "330px",
-                }}
-            >
-                <div className="signup__container">
-                    <br></br>
-                    <h2>Rekisteröidy</h2>
-                    <form className="signup__form" onSubmit={handleSubmit}>
-                        <div>
-                            <input
-                                type="email"
-                                name="sposti"
-                                id="sposti"
-                                value={sposti}
-                                placeholder="Syötä sähköposti"
-                                required
-                                onChange={(e) => setSposti(e.target.value)}
-                            />
-                        </div>
-                        <br></br>
-                        <div>
-                            <input
-                                type="text"
-                                id="kayttajanimi"
-                                name="kayttajanimi"
-                                value={kayttajanimi}
-                                placeholder="Syötä käyttäjänimi"
-                                required
-                                onChange={(e) =>
-                                    setKayttajanimi(e.target.value)
-                                }
-                            />
-                        </div>
-                        <br></br>
-                        <div>
-                            <input
-                                type="password"
-                                name="salasana"
-                                id="salasana"
-                                minLength={8}
-                                value={salasana}
-                                placeholder="Syötä salasana"
-                                required
-                                onChange={(e) => setSalasana(e.target.value)}
-                            />
-                        </div>
-                        <br></br>
-                        <div>
-                            <button data-testid="rek">Rekisteröidy</button>
-                        </div>
-                        <div>
-                            <p>
-                                Sinulla on jo tili?{" "}
-                                <Nav.Link
-                                    href="/"
-                                    to="/"
-                                    style={{
-                                        color: "blue",
-                                        textDecoration: "underline",
-                                    }}
-                                >
-                                    Palaa etusivulle.
-                                </Nav.Link>
-                            </p>
-                        </div>
-                    </form>
-                </div>
-            </div>)} */
